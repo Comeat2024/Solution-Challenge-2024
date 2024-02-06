68f670c44bdaeef66267330269751bc03b785f96
suyong choi <dpfprtus@gmail.com>
2024-02-06 17:18:55 +0900

    [FEAT] Splash Page(카카오 로그인 버튼 수정 필요)

--- jeogongtong_front/lib/main.dart
+++ jeogongtong_front/lib/main.dart
@@ -19,9 +19,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffF7CCDB))
+            .copyWith(background: const Color(0xffF7CCDB)),
       ),
       home: const SplashPage(),
     );
--- jeogongtong_front/lib/pages/splash_page.dart
+++ jeogongtong_front/lib/pages/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -32,48 +33,81 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(
-        title: const Text("Main Page"),
-        centerTitle: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Center(
-            child: Text(
-              "효율적인 공부 관리 어플",
+      body: Padding(
+        padding: const EdgeInsets.only(top: 48),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "공부의 저금고,",
+                style: TextStyle(
+                  fontSize: 48,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Text(
+              "저공통",
               style: TextStyle(
-                fontSize: 30,
+                fontSize: 48,
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Center(
-            child: Text(
-              "쉽고 빠르게 시작해보세요",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            const SizedBox(
+              height: 48,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              Flexible(
+                child: SvgPicture.asset(
+                  "assets/images/app_icon.svg",
+                ),
+              ),
+            ]),
+            const SizedBox(
+              height: 5,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                signInWithGoogle(context);
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.zero, // 패딩 제거
+              ),
+              child: SvgPicture.asset(
+                "assets/images/google_login.svg",
               ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              signInWithGoogle(context);
-            },
-            child: const Text(
-              "구글 로그인",
-              style: TextStyle(color: Colors.black),
+            const SizedBox(
+              height: 5,
             ),
-          )
-        ],
+            ElevatedButton(
+              onPressed: () {
+                signInWithGoogle(context);
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.zero,
+              ),
+              child: SvgPicture.asset(
+                "assets/images/kakao_login.svg",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                signInWithGoogle(context);
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.zero, // 패딩 제거
+              ),
+              child: SvgPicture.asset(
+                "assets/images/naver_login.svg",
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
